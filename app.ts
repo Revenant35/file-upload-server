9e91f5b5b424e64fb17aa49cf348e32574b24179
Zach Brown <zach.brown@illumica.co>
2024-03-26 19:37:15 -0500

    fixing logging

--- app.ts
+++ app.ts
@@ -18,6 +18,6 @@ config();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server running on port 3000");
+  console.log(`Server running on port ${PORT}`);
 });
 
