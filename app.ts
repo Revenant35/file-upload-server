145f71f49659dd8967f50d394aa39b47ded2528f
Zach Brown <zach.brown@illumica.co>
2024-03-27 00:43:39 -0500

    adding /files/ to main path; updating dependencies

--- app.ts
+++ app.ts
@@ -5,13 +5,17 @@ import helmet from "helmet";
 import {config} from "dotenv";
 
 import router from "./router";
+import {notFoundHandler} from "./src/middleware/not-found";
+import {errorHandler} from "./src/middleware/error";
 
 const app = express();
 app.use(cors());
 app.use(compression());
 app.use(helmet());
 
-app.use(router);
+app.use("/files", router);
+app.all("*", notFoundHandler);
+app.use(errorHandler);
 
 config();
 
